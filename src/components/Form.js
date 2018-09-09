96b4f9dfc2d31713bccf7974de5aef3b30fb3b92
adityapsvs <adityapsvs@gmail.com>
2018-09-09 23:58:16 +0530

    localStorage done right. But preventDefault not allowing render

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,29 +11,39 @@ export default class Form extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			date: moment(),
-			time: moment(),
+			eventDate: moment(),
+			eventTime: moment(),
 			allDay: false,
 			eventName: '',
-			location: ''
+			eventLocation: ''
 		}
 	}
 
-	handleDate = date => this.setState({ date: date })
-	handleTime = time => this.setState({ time })
+	handleDate = date => this.setState({ eventDate: date })
+	handleTime = time => this.setState({ eventTime: time })
 	changeAllDay = () => {
 		let allDay = this.state.allDay
 		this.setState({ allDay: !allDay })
 	}
 	handleEventName = event => this.setState({ eventName: event.target.value })
-	setLocation = location => this.setState({ location: location })
+	setLocation = location => this.setState({ eventLocation: location })
 	handleSubmit = (e) => {
 		e.preventDefault()
-		console.log(moment(this.state.date).format('DD/MM/YYYY'))
-		console.log(moment(this.state.time).format('HH:mm:ss'))
-		console.log(this.state.allDay)
-		console.log(this.state.eventName)
-		console.log(this.state.location)
+		let currEventDetails = this.state
+		currEventDetails.eventDate = moment(this.state.eventDate).format('DD/MM/YYYY')
+		if(this.state.allDay) currEventDetails.eventTime = 'All Day'
+		else currEventDetails.eventTime = moment(this.state.eventTime).format('HH:mm:ss')
+		let prevEventDetails = {}
+		if(localStorage.getItem('eventDetails'))
+			prevEventDetails = JSON.parse(localStorage.getItem('eventDetails'))
+		localStorage.setItem('eventDetails', JSON.stringify([...prevEventDetails, currEventDetails]))
+		this.setState({
+			eventDate: moment(),
+			eventTime: moment(),
+			allDay: false,
+			eventName: '',
+			eventLocation: ''
+		})
 	}
 
   render() {
@@ -44,33 +54,37 @@ export default class Form extends Component {
           <label>Event Name</label>
           <input placeholder='Event Name' onChange={e => this.handleEventName(e)} />
         </div>
-        <div>
-          <DatePicker onChange={this.handleDate} selected={this.state.date} />
+        <div className='field'>
+					<label>Date</label>
+          <DatePicker onChange={this.handleDate} selected={this.state.eventDate} />
         </div>
-				<div className='ui divided two column grid'>
-					<div className='row'>
-						<div className='column'>
-							<div className='field'>
-			          <div className='ui checkbox'>
-			            <input type='checkbox' onChange={this.changeAllDay} />
-			            <label>All day?</label>
-			          </div>
-			        </div>
-						</div>
-						<div className='column' >
-							{
-								!this.state.allDay ?
-									<TimePicker defaultValue={this.state.time} onChange={this.handleTime} />
-								:
-									null
-							}
+				<div className='field'>
+					<label>Time</label>
+					<div className='ui divided two column grid'>
+						<div className='row'>
+							<div className='column'>
+								<div className='field'>
+				          <div className='ui checkbox'>
+				            <input type='checkbox' onChange={this.changeAllDay} />
+				            <label>All day?</label>
+				          </div>
+				        </div>
+							</div>
+							<div className='column' >
+								{
+									!this.state.allDay ?
+										<TimePicker defaultValue={this.state.eventTime} onChange={this.handleTime} />
+									:
+										null
+								}
+							</div>
 						</div>
 					</div>
 				</div>
 				<div className='field'>
 					<label>Location</label>
 					{/* <input placeholder='Location' /> */}
-					<Location address={this.state.location} handleChange={this.setLocation} />
+					<Location address={this.state.eventLocation} handleChange={this.setLocation} />
 				</div>
         <button type='submit' className='ui button' role='button'>
           Submit
